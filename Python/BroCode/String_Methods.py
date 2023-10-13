d07429b6cd64bef0c421a760d44864fc52a284f6
JATIN KISHORE M <122717391+JatinKishore@users.noreply.github.com>
2023-10-13 14:41:27 +0530

    Create String_Methods.py

---
+++ Python/BroCode/String_Methods.py
@@ -0,0 +1,23 @@
+name = "bro code"
+print(len(name)) # length
+
+print(name.find("b")) # to find first occurence of a character
+
+print(name.capitalize()) # to capitalize the first letter in first word
+
+
+print(name.upper()) # upperCase all the letters
+
+print(name.lower()) # lowerCase all the letters
+
+print(name.isdigit()) # check string contains only numbers
+
+print(name.isalpha()) # check string contains only alphabets
+
+print(name.count("o")) # check for numbers of occurence of a specific character
+
+print(name.replace("o","a")) # to replace a character with another character
+
+print(name*3) # it is not a method but used to print a string n times
+
+
