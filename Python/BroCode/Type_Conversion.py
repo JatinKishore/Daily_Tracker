b658e93ae25130e1c7b2f4c8d053bf4aef3fd93a
JATIN KISHORE M <122717391+JatinKishore@users.noreply.github.com>
2023-10-13 14:41:53 +0530

    Create Type_Conversion.py

---
+++ Python/BroCode/Type_Conversion.py
@@ -0,0 +1,25 @@
+x = 1 #int
+y = 2.0 #float
+z = "3" #str
+
+print(x)
+print(y)
+print(z)
+
+y=int(y)
+print(y)
+
+z = int(z)
+
+print(z)
+
+x = float(x)
+
+print(x)
+
+x=str(x)
+
+print(x)
+
+# print("The value of x is "+str(x))  --> this is an error ..we cant concat a string with a integer
+print("The value of x is "+str(x))
