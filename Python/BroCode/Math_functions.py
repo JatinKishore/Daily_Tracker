dfe3d2134bd42d7867bc0193382672d80cf50177
JATIN KISHORE M <122717391+JatinKishore@users.noreply.github.com>
2023-10-13 14:42:35 +0530

    Create Math_functions.py

---
+++ Python/BroCode/Math_functions.py
@@ -0,0 +1,19 @@
+import math # to use math functions we need to include math library
+
+pi = 3.14
+
+print(round(pi)) # round off the number
+
+print(math.ceil(pi)) # it round up the number
+
+print(math.floor(pi)) # round down the number
+
+print(abs(pi)) # gives the positive number
+
+print(pow(pi,2)) #power of the number
+
+print(math.sqrt(pi)) #square root and it won't acccept negative numbers
+
+print(max(1,2,3)) # highest number among all
+
+print(min(1,2,3)) # lowest number among all
