1fe5e1ac03a6642ad0b8c1ffb23688633eecddb5
JATIN KISHORE M <122717391+JatinKishore@users.noreply.github.com>
2023-10-13 14:42:16 +0530

    Create Getting_input.py

---
+++ Python/BroCode/Getting_input.py
@@ -0,0 +1,9 @@
+name = input("What is your name?: ") #whenever we accept an input it is of the type basestring
+print("Hello "+ name)
+
+age = int(input("How old are you")) # getting integer as input
+height = float(input("How tall your?"))
+print("the age {} is correct".format(age))
+
+print(age+1)
+print(height)
