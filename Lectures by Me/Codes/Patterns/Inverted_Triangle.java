ef2f91c2677eba78a0b1c1bf6b2cdda63ec66742
JATIN KISHORE M <122717391+JatinKishore@users.noreply.github.com>
2023-10-14 09:00:19 +0530

    Update Inverted_Triangle.java

--- Lectures by Me/Codes/Patterns/Inverted_Triangle.java
+++ Lectures by Me/Codes/Patterns/Inverted_Triangle.java
@@ -1,12 +1,5 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-public class Main
+public class Inverted_Triangle
 {
 	public static void main(String[] args) {
 	    for(int i=4;i>=1;i--){
