f71da59865490ecf7317b37407eef4ed9fa2fb96
JATIN KISHORE M <122717391+JatinKishore@users.noreply.github.com>
2023-10-14 09:12:23 +0530

    Create square.java

---
+++ Lectures by Me/Codes/Patterns/square.java
@@ -0,0 +1,55 @@
+
+import java.util.*;
+public class square
+{
+     public static void main(String[] args) {
+
+             for(int i=1;i<=4;i++){
+                 for(int j=1;j<=4;j++){
+                     System.out.print("* ");
+                 }
+                 System.out.println();
+             }
+     }
+}
+
+/*
+Numerical Square
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		int k=1;
+		for(int i=1;i<=4;i++){
+		    
+		    for(int j=1;j<=4;j++){
+		        System.out.print(k++ +" ");
+		    }
+		    
+		    System.out.println();
+		}
+	}
+}
+*/
+
+/*
+
+Character Square
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		char k=65;
+		for(int i=1;i<=4;i++){
+		    
+		    for(int j=1;j<=4;j++){
+		        System.out.print(k++ +" ");
+		    }
+		    
+		    System.out.println();
+		}
+	}
+}
+
+*/
