feef99b2c9fad555204d2e1de2ad1d3038771e82
JATIN KISHORE M <122717391+JatinKishore@users.noreply.github.com>
2023-10-14 09:01:45 +0530

    Update rectangle.java

--- Lectures by Me/Codes/Patterns/rectangle.java
+++ Lectures by Me/Codes/Patterns/rectangle.java
@@ -1,13 +1,5 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 import java.util.*;
-public class Main
+public class rectangle
 {
 	public static void main(String[] args) {//L=5;B=6
 		
