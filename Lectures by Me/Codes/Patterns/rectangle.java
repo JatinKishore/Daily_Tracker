e92b6f8826688abd35caef2af5efdd9df86d20f7
JATIN KISHORE M <122717391+JatinKishore@users.noreply.github.com>
2023-10-14 08:55:43 +0530

    Create rectangle.java

---
+++ Lectures by Me/Codes/Patterns/rectangle.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {//L=5;B=6
+		
+		for(int i=1;i<=6;i++){
+		    for(int j=1;j<=5;j++){
+		        System.out.print("* ");
+		    }
+		    System.out.println();
+		}
+	}
+}
