cc033082da368c905706774e18c0d0898f8bf9b1
JATIN KISHORE M <122717391+JatinKishore@users.noreply.github.com>
2023-10-14 09:11:29 +0530

    Create triangle.java

---
+++ Lectures by Me/Codes/Patterns/triangle.java
@@ -0,0 +1,44 @@
+public class triangle
+{
+	public static void main(String[] args) {
+	    
+		for(int i=1;i<=4;i++){
+		    for(int j=1;j<=i;j++){
+		        System.out.print("* ");
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+/*
+Numerical Triangle
+public class Main
+{
+	public static void main(String[] args) {
+	    int k =1;
+		for(int i=1;i<=4;i++){
+		    for(int j=1;j<=i;j++){
+		        System.out.print(k++ +" ");
+		    }
+		    System.out.println();
+		}
+	}
+}
+*/
+
+/*
+Character Triangle
+public class Main
+{
+	public static void main(String[] args) {
+	    char k =65;
+		for(int i=1;i<=4;i++){
+		    for(int j=1;j<=i;j++){
+		        System.out.print(k++ +" ");
+		    }
+		    System.out.println();
+		}
+	}
+}
+*/
