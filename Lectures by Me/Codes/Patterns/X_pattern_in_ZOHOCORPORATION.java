861be785158fbdaf129090127b6ccbbfd4439d7c
JATIN KISHORE M <122717391+JatinKishore@users.noreply.github.com>
2023-10-14 09:01:17 +0530

    Update X_pattern_in_ZOHOCORPORATION.java

--- Lectures by Me/Codes/Patterns/X_pattern_in_ZOHOCORPORATION.java
+++ Lectures by Me/Codes/Patterns/X_pattern_in_ZOHOCORPORATION.java
@@ -1,13 +1,6 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
+*/
 import java.util.*;
-public class Main
+public class X_pattern_in_ZOHOCORPORATION
 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
