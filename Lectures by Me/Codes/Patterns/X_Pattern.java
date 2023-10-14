00c5dc89a5b7c3e02c652b017b8fb13fc660a1f2
JATIN KISHORE M <122717391+JatinKishore@users.noreply.github.com>
2023-10-14 08:57:28 +0530

    Create X_Pattern.java

---
+++ Lectures by Me/Codes/Patterns/X_Pattern.java
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    for(int i=1;i<=n;i++){
+	        for(int j=1;j<=n;j++){
+	            if(j==i || j==n-i+1){
+	                System.out.print("*");
+	            }
+	            else{
+	                System.out.print(" ");
+	            }
+	        }
+	        System.out.println();
+	    }
+	}
+}
